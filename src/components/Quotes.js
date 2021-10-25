6d4cf7412afb04560fc95e0dee3871de6bfa2051
ahmetterturk <ahm.ert@hotmail.com>
2021-10-25 17:15:08 +1100

    get quote on click

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,10 +3,8 @@ import React, { useEffect, useState } from 'react';
 const Quotes = () => {
   const [quote, setQuote] = useState('');
   const [name, setName] = useState('');
-  const [quoteAuthorId, setQuoteAuthorId] = useState('');
 
-
-  useEffect(() => {
+  const getQuote = () => {
     fetch(`https://www.breakingbadapi.com/api/quote/random`)
       .then((response) => response.json())
       .then((data) => {
@@ -14,37 +12,18 @@ const Quotes = () => {
         setName(data[0].author);
       })
       .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getQuote();
   }, []);
 
-  
-  const authorName = name
-  // console.log(authorName)
-
-  if (name.length > 0) {
-    fetch(`https://www.breakingbadapi.com/api/characters`)
-    .then(response => response.json())
-    .then(data => {})
-    .catch(error => console.log(error))
-  } 
-
-
-
-  // console.log(name.split(" ").join('+'))
-
-  //   const getCharacterId = () => {
-  //     console.log(quote.author);
-  //     fetch(`https://www.breakingbadapi.com/api/characters?name=${quote.author.split(' ').join('+')}`)
-  //       .then((response) => response.json())
-  //       .then((data) => console.log(data));
-  //     // console.log(quote.author);
-  //   };
-
-  return (    
+  return (
     <div>
       <h1>Quotes</h1>
       <h2>{quote.quote}</h2>
       <h3>{quote.author}</h3>
-      {/* <p>{quoteAuthorId}</p> */}
+      <input type="button" onClick={getQuote} value="Get Quote" />
     </div>
   );
 };
