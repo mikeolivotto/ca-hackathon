45337f09b6f198b4f99ff6327cb6e6a043a720ed
ahmetterturk <ahm.ert@hotmail.com>
2021-10-25 15:00:44 +1100

    trying to set the name

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -2,13 +2,32 @@ import React, { useEffect, useState } from 'react';
 
 const Quotes = () => {
   const [quote, setQuote] = useState('');
+  const [name, setName] = useState('');
+  const [quoteAuthorId, setQuoteAuthorId] = useState('');
+
+  //   const authorName = quote.author.split(' ').join('+');
+  //   console.log(quote.author.split(' ').join('+'));
 
   useEffect(() => {
     fetch(`https://www.breakingbadapi.com/api/quote/random`)
       .then((response) => response.json())
-      .then((data) => setQuote(data[0]))
+      .then((data) => {
+        setQuote(data[0]);
+        setName(data[0].author);
+        console.log(name);
+        return name;
+      })
+      .then((name) => console.log(name))
       .catch((error) => console.log(error));
-  }, []);
+  }, [name]);
+
+  //   const getCharacterId = () => {
+  //     console.log(quote.author);
+  //     fetch(`https://www.breakingbadapi.com/api/characters?name=${quote.author.split(' ').join('+')}`)
+  //       .then((response) => response.json())
+  //       .then((data) => console.log(data));
+  //     // console.log(quote.author);
+  //   };
 
   return (
     <div>
